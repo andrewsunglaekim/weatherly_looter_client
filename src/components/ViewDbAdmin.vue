d11d82f46d4d529ba7bc3f4aa8df7a007d6ad44e
Andy <andrew.sunglae.kim@gmail.com>
2018-09-12 23:35:30 -0400

    bootstraps intial db admin view

---
+++ src/components/ViewDbAdmin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="view-db-admin">
+    Admin Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewDbAdmin',
+};
+</script>
+
+<style lang="css">
+  .view-db-admin{
+
+  }
+</style>
